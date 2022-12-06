716a5ef058d27329dc9edf4ab144928feb2c8f45
Rosemary Roach <rosemary@dopeapps.com>
2022-12-06 14:00:42 -0800

    testing

--- myTurtle.lua
+++ myTurtle.lua
@@ -40,11 +40,11 @@ end
 
 function myTurtle.move(...)
 	if arg[2] == nil then
-		print("1")
+		shell.run("label set 1")
 		moveOnlyDirection(arg[1])
 	else
 		moveWithNumber(arg[1], arg[2])
-		print("2")
+		shell.run("label set 2")
 	end
 end
 
