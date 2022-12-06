168cb041d60701c9e499f2fcf2ea440bc27b74b5
Rosemary Roach <rosemary@dopeapps.com>
2022-12-06 12:28:50 -0800

    Added comments and fixed bug

--- start.lua
+++ start.lua
@@ -1,7 +1,18 @@
+
+-- Clone the repo to CCScripts (Could check version at some point)
 shell.run("github clone RosesAreMerry/CCScripts -b main")
+
+-- delete old scripts folder and rename. (So that files actually get deleted when they are no longer in git)
+shell.run("delete scripts")
+shell.run("rename CCScripts scripts")
+
+-- Replace current startup with new startup file from git. Rename currently running file.
 shell.run("cp CCScripts/start.lua /")
 shell.run("rename startup.lua oldstart.lua")
 shell.run("rename start.lua startup.lua")
+
+-- Enter program
 shell.run("CCScripts/entry.lua")
+
+-- Delete current file.
 shell.run("delete oldstart.lua")
--- Does this get copied?
