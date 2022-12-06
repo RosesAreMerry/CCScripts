55f4771e485fc4fb7f38a1a28c669490108d5042
Rosemary Roach <rosemary@dopeapps.com>
2022-12-06 14:23:33 -0800

    testing

--- myTurtle.lua
+++ myTurtle.lua
@@ -50,7 +50,7 @@ end
 function myTurtle.findItem(a)
 	for i = 1, 16 do
 		turtle.select(i)
-		if turtle.getItemDetail().name == ("minecraft:" + a) then
+		if turtle.getItemDetail().name == ("minecraft:" .. a) then
 			return
 		end
 	end
