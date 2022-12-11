b36310030b3e5b1bb53bc0da44d63bcdf7b501f5
Rosemary Roach <rosemary@dopeapps.com>
2022-12-11 10:04:46 -0800

    testing

--- turtleMain.lua
+++ turtleMain.lua
@@ -17,11 +17,13 @@ if (t.checkFuel(100)) then
 	print("outside checkTorches")
 	while running do
 		if t.checkTorches() then
+			print("making player tunnel")
 			t.turn(Direction.left)
 			t.playerTunnel(30)
 			t.moveTurn(Direction.backward, 30)
 			t.turn(Direction.left)
 		else
+			print("making hallway")
 			t.move(Direction.right)
 			while not(turtle.detect()) do
 				t.move(Direction.forward)
