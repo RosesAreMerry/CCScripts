d69bcea81d91c4170f5c32733a1d1c597808c3b1
Rosemary Roach <rosemary@dopeapps.com>
2022-12-10 20:16:34 -0800

    yay

--- myTurtle.lua
+++ myTurtle.lua
@@ -1,6 +1,6 @@
 local t = {}
 
-facingDirection = Direction.forward
+require("Direction")
 
 --- Wrapper for turtle.turn functions using Direction class. It will only work for directions
 --- that can be turned to; left, right, and backward.
