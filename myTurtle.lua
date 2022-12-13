0265ac2b7bcdaf8907f01208c9843bbcc4d51292
Rosemary Roach <rosemary@dopeapps.com>
2022-12-12 16:47:39 -0800

    testing

--- myTurtle.lua
+++ myTurtle.lua
@@ -235,6 +235,7 @@ end
 ---@param hallwayLength number
 ---@return boolean
 function t.checkTunnels(hallwayLength)
+	if (hallwayLength == nil) then hallwayLength = 30
 	for i = 0, hallwayLength do
 		t.move(Direction.forward)
 		if i % 5 == 0 then
